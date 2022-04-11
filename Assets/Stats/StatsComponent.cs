5910a687a7d75da57075b4df4b9357da5d975cda
G3rw4nt <krzysztof.dereszynski@gmail.com>
2022-04-11 18:15:40 +0200

    deleted useless code

--- Assets/Stats/StatsComponent.cs
+++ Assets/Stats/StatsComponent.cs
@@ -12,32 +12,5 @@ public class StatsComponent : MonoBehaviour
     {
         healthBar.Setup(health);
         Debug.Log(health.GetStatName()+": "+health.GetCurrentAmount());
-
-        CMDebug.ButtonUI(new Vector2(100,100), "damage", () => {
-            health.SubstractAmount(10);
-            Debug.Log(health.GetStatName()+": "+health.GetAmountPercentage());
-        });
-        CMDebug.ButtonUI(new Vector2(-100,100), "heal", () => {
-            health.AddAmount(10);
-            Debug.Log(health.GetStatName()+": "+health.GetAmountPercentage());
-        });
     }
-    // float time = 0;
-    // void Update() { 
-    //     time += Time.deltaTime;  
-    //     if(time > wait)
-    //     {
-    //         if(Input.GetKey(KeyCode.Q)) 
-    //         {
-    //             health.substractAmount(10);
-    //             Debug.Log(health.getStatName()+": "+health.getCurrentAmount());
-    //         };
-    //         if(Input.GetKey(KeyCode.E))
-    //         {
-    //             health.addAmount(10);
-    //             Debug.Log(health.getStatName()+": "+health.getCurrentAmount());
-    //         };
-    //     }
-    // }
-
 }
