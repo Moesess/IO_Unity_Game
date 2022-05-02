ecebcb32a6f4d9fb1306fd622651d048355f5991
G3rw4nt <krzysztof.dereszynski@gmail.com>
2022-05-03 00:28:01 +0200

    player movement and camera follow improved

---
+++ Assets/PlayerController/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float speed = 5;
+    [SerializeField] private float turnSpeed = 360;
+    private Vector3 input;
+    private Animation anim;
+
+    void Update()
+    {
+        
+        GatherInput();
+        Look();
+    }
+
+    void FixedUpdate() {
+        if(input.x != 0 || input.z != 0)
+        {
+            Move();
+        }
+    }
+
+    void GatherInput()
+    {
+        input = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+    }
+
+    void Look()
+    {
+        if(input != Vector3.zero)
+        {
+            var relative = (transform.position + input.ToIso()) - transform.position;
+            var rot = Quaternion.LookRotation(relative,Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, turnSpeed * Time.deltaTime);
+        }
+    }
+
+    void Move()
+    {
+        rb.MovePosition(transform.position + (transform.forward * input.magnitude) * speed * Time.deltaTime);
+    }
+}
